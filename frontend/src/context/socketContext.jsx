37d4d8b6f716806604080410c1b4cfaf8eb35797
77Mehdi <mgharyb917@gmail.com>
2024-03-30 20:38:54 +0000

    Socket url updat

--- frontend/src/context/socketContext.jsx
+++ frontend/src/context/socketContext.jsx
@@ -20,7 +20,8 @@ export const SocketContextProvider = ({children})=>{
 
     useEffect(()=>{
         if(authUser){
-            const socket = io('http://localhost:8000',{
+            // const socket = io('http://localhost:8000',{
+                const socket = io('https://mern-chat-app-q8cn.onrender.com',{
                 query:{
                     userId : authUser.user._id
                 }
